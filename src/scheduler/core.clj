6aa8f19450a28f52a9b831cdd6e0c7be774b6c0d
Tomasz Gac <tomasz.k.gac@gmail.com>
2021-12-03 15:58:06 +0100

    process implementation

--- src/scheduler/core.clj
+++ src/scheduler/core.clj
@@ -2,36 +2,48 @@
   (:require [clojure.core.logic :as l])
   (:require [clojure.core.logic.fd :as fd]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
-(defn worko
+(l/defne worko
   "Defines work relationship. Work start must be before end, and have a duration."
-  [s d e]
-  (l/all (fd/< s e) (fd/+ s d e)))
+  [work] ([[s d e]] (l/all (fd/< s e) (fd/+ s d e))))
 
 (l/defne processo
-  "Defines a process. Process is a relationship between three lists that define a series of work.
-  In a list of work, next work cannot start, before previous was finished."
-  [starts ends]
-         ([[] []])
-         ([[s] [e]])
-         ([[s . s2 . sd] [e . e2 . ee]]
-          (fd/<= s2 e)
-          (processo (l/llist s2 sd) (l/llist e2 ee))))
+  "Defines a process goal.
+  Process is a list of work, in which next work cannot start before previous has ended"
+  [works]
+         ([[]])
+         ([[a]])
+         ([[a b . d]]
+          (l/matche [a b] ([ [_ _ e] [s _ _] ] (fd/<= e s)))
+          (processo (l/llist b d))))
 
-(defn keyorfresho [map key]
+(defn keyorfresho
+  "Returns a goal that assigns a value from a map at a given key,
+  or a fresh value if the key is missing"
+  [map key]
   (let [v (key map)]
     (if (nil? v)
       (fn [x] (l/fresh [s] (l/== s x)))
       (fn [x] (l/== v x)))))
 
-(defn extract-work
+(defn is-work
+  "Returns a goal that unifies x with work data specified by the supplied map.
+  Work is a list [start duration end] that corresponds with map values at those keys."
   [work]
   (l/fne [x]
     ([[s d e]]
      ((keyorfresho work :start) s)
      ((keyorfresho work :duration) d)
-     ((keyorfresho work :end) e))))
+     ((keyorfresho work :end) e)
+     )))
+
+(defn is-process
+  "Returns a goal that unifies x with process data specified by the supplied collection of maps.
+  Process is a list of work data, as defined by is-work."
+  ([[p & process]]
+   (if (empty? process)
+     (fn [x] l/succeed)
+     (l/fne [x]
+            ([[a . d]] ((is-work p) a) ((is-process process) d))))))
+
+
+
