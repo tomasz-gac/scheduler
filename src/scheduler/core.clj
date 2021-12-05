019fdab6fab99d8a4db02ab1ee5ad7dfc7003752
Tomek Vivobook <tomasz.k.gac@gmail.com>
2021-12-05 20:21:32 +0100

    Space constraint

--- src/scheduler/core.clj
+++ src/scheduler/core.clj
@@ -4,19 +4,32 @@
 
 (l/defne worko
   "Defines work relationship. Work start must be before end, and have a duration."
-  [work] ([[s d e]] (l/all (fd/< s e) (fd/+ s d e))))
+  [work] ([[s d e]]
+          (l/all (fd/< s e) (fd/+ s d e))))
+
+(l/defne happens-before
+         "Defines a happens-before relationship between two works."
+         [before after]
+         ([[_ _ e] [s _ _]] (fd/<= e s)))
+
+(l/defne non-overlapping
+         [a b]
+         ([[s1 _ e1] [s2 _ e2]]
+          (l/conde
+            [(fd/>= s2 e1)]
+            [(fd/>= s1 e2)])))
 
 (l/defne processo
   "Defines a process goal.
   Process is a list of work, in which next work cannot start before previous has ended"
   [works]
-         ([[]])
-         ([[a]])
-         ([[a b . d]]
-          (l/matche [a b] ([ [_ _ e] [s _ _] ] (fd/<= e s)))
-          (processo (l/llist b d))))
+  ([[]])
+  ([[a]])
+  ([[a b . d]]
+   (l/matche [a b] ([ [_ _ e] [s _ _] ] (fd/<= e s)))
+   (processo (l/llist b d))))
 
-(defn keyorfresho
+(defn key-or-fresho
   "Returns a goal that assigns a value from a map at a given key,
   or a fresh value if the key is missing"
   [map key]
@@ -31,19 +44,17 @@
   [work]
   (l/fne [x]
     ([[s d e]]
-     ((keyorfresho work :start) s)
-     ((keyorfresho work :duration) d)
-     ((keyorfresho work :end) e)
-     )))
+     ((key-or-fresho work :start) s)
+     ((key-or-fresho work :duration) d)
+     ((key-or-fresho work :end) e)
+     (is-work [s d e]))))
+
+
 
 (defn is-process
   "Returns a goal that unifies x with process data specified by the supplied collection of maps.
-  Process is a list of work data, as defined by is-work."
+  Process is an ordered list of work data, as defined by is-work."
   ([[p & process]]
    (if (empty? process)
-     (fn [x] l/succeed)
-     (l/fne [x]
-            ([[a . d]] ((is-work p) a) ((is-process process) d))))))
-
-
-
+     (l/fne [x] ([_] ((is-work p) x)))
+     (l/fne [x] ([[a . [d]]] ((is-work p) a) ((is-process process) d))))))
